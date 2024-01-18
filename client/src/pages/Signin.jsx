24f68f882d13fcba36389de3e2b6939edf3585f7
sumedha-Niroshan <sumedhan199@gmail.com>
2024-01-18 08:54:53 +0530

    Compelete sign in page functinality

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,5 +1,78 @@
-const SignIn = () => {
-  return <div>sign in</div>;
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const SignUp = () => {
+  const [fromData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...fromData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth//sing-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(fromData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSumbit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading" : "sign in"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't Have an account</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 };
 
-export default SignIn;
+export default SignUp;
