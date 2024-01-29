f7d3fcf328175fe09153bd53f9331cb6bd08e447
sumedha-Niroshan <sumedhan199@gmail.com>
2024-01-29 20:51:33 +0530

    add pre installtions

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import authRouter from "./routers/Auth.router.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routers/listing.route.js";
+import path from "path";
 
 mongoose
   .connect(process.env.MONGO)
@@ -16,6 +17,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -31,6 +34,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", " index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
