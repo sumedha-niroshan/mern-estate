4e396d4ebf3a50831110b7a930f9e50b53aa008c
sumedha-Niroshan <sumedhan199@gmail.com>
2024-12-24 16:35:19 +0530

    Change about us page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,14 +1,35 @@
 const About = () => {
-  return <div className="py-20 px-4 max-w-6xl mx-auto flex flex-col gap-7">
-    <h1 className="text-3xl font-bold mb-4 text-slate-800 ">About Ikman Estate</h1>
-    <p className="text-slate-700">Step into a world of unparalleled variety and convenience with Ikman Estate. We redefine the way you shop by offering an extensive array of top-quality products and services all in one place. Whether you're seeking the latest fashion trends, cutting-edge electronics, or unique handmade crafts, our marketplace is your ultimate destination.</p>
-    <p className="text-slate-700">
-    At Ikman Estate, we prioritize your satisfaction and aim to create a seamless shopping experience. Our platform is designed to be user-friendly, allowing you to effortlessly navigate through a diverse range of offerings. With a commitment to quality, we bring together trusted sellers and showcase their best products to ensure you receive nothing but excellence.
-    </p>
-    <p className="text-slate-700">
-    Expect exclusive deals, exciting promotions, and personalized recommendations to enhance your shopping journey. We believe in making every purchase a delight, and our global community of sellers ensures you have access to unique items with a local touch.Join us at Ikman Estate and transform the way you shop. Explore, indulge, and find the perfect match for your needs. Your next great discovery awaits!
-    </p>
-  </div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto flex flex-col gap-7">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 ">
+        About Ikman Estate
+      </h1>
+      <p className="text-slate-700">
+        Step into a world of unparalleled variety and convenience with Ikman
+        Estate. We redefine the way you shop by offering an extensive array of
+        top-quality products and services all in one place. Whether you're
+        seeking the latest fashion trends, cutting-edge electronics, or unique
+        handmade crafts, our marketplace is your ultimate destination.
+      </p>
+      <p className="text-slate-700">
+        At Ikman Estate, we prioritize your satisfaction and aim to create a
+        seamless shopping experience. Our platform is designed to be
+        user-friendly, allowing you to effortlessly navigate through a diverse
+        range of offerings. With a commitment to quality, we bring together
+        trusted sellers and showcase their best products to ensure you receive
+        nothing but excellence.
+      </p>
+      <p className="text-slate-700">
+        Expect exclusive deals, exciting promotions, and personalized
+        recommendations to enhance your shopping journey. We believe in making
+        every purchase a delight, and our global community of sellers ensures
+        you have access to unique items with a local touch.Join us at Ikman
+        Estate and transform the way you shop. Explore, indulge, and find the
+        perfect match for your needs. Your next great discovery awaits!
+      </p>
+      <p className="text-slate-700">And this project for testing</p>
+    </div>
+  );
 };
 
 export default About;
